bd8bcb39cd05e248f50ec0f229cae3277279b54d
vojta001 <vojta001@e6a2024a-f257-11e3-b826-311ed6531c3a>
2014-09-25 17:59:04 +0000

    validace a ukládání editů do $_SESSION, příprava na js validování

--- page/home/prepare.php
+++ page/home/prepare.php
@@ -7,6 +7,22 @@ $jsScripts[] = JS_PATH.'mathquill.min.js';
 $cssStyles[] = CSS_PATH.'mathquill.css';
 
 
+function addDeciamlJS() {
+	$q = getCurrentQ();
+	$raise = FALSE;
+	foreach ($q->answer as $a) {
+		if ($a->typ == AT_EDIT) {
+			$raise = TRUE;
+			break;
+		}
+	}
+	if ($raise)
+		$jsScripts[] = JS_PATH.'validatedecimals.js';
+}
+
+addDeciamlJS();
+
+
 function spockSay($text, $left = FALSE) {
   $out = '';
   if ($left)
@@ -27,7 +43,7 @@ function renderOtazka($q) {
     case QT_OBR:
       $x = '<p>'.$q->data2.'</p><img src="'.IMG_PATH.'q/'.$q->data.'" alt="otázka" />';
       break;
-    case QT_MATH:                 
+    case QT_MATH:
       $x = '<p>'.$q->data2.'</p><span class="mathquill-embedded-latex">'.$q->data.'</span>';
       break;
     default:
@@ -35,7 +51,7 @@ function renderOtazka($q) {
       $x = '<p class="error">Zobrazení není definováno!</p>';
       break;
 	}
-  return '<div class="otazka">'.$x.'</div>';	
+  return '<div class="otazka">'.$x.'</div>';
 }
 
 
@@ -51,12 +67,12 @@ function renderOdpoved($q) {
     	case AT_OBR:
     		$x = '<img src="'.IMG_PATH.'a/'.$a->data.'" alt="otázka" />';
       	break;
-    	case AT_MATH:                 
+    	case AT_MATH:
     	  $x = '<span class="mathquill-embedded-latex">'.$a->data.'</span>';
       	break;
-    	case AT_EDIT:                 
-    	  $x = '<span>'.$a->data.'</span><input type="text" name="edit-'.$i.'" value="" />';
-      	break;
+    	case AT_EDIT:
+				$x = '<span>'.$a->data.'</span><input class="decimalTextBox" type="text" name="edit-'.$i.'" value="'.$a->odpovedDecimal.'" />';
+				break;
       default:
         flm('Není definováno chování pro odpověď typu '.$a->typ, '', MSG_ERROR);
 				flm (print_r ($a, 1));
@@ -73,16 +89,16 @@ function renderOdpoved($q) {
 		else
 			$out .= '<li><input type="radio" name="moznost" value="'.$i.'" '.$checked.'/>'.$x.'</li>';
 	}
-	
+
   return '<div class="odpoved"><ul>'.$out.'</ul></div>';
-} 
+}
 
 
 function renderQ() {
-	$q = getCurrentQ();   
-	$qn = getCurrentQnum();   
+	$q = getCurrentQ();
+	$qn = getCurrentQnum();
 	$out = renderotazka($q);
-	$out .= renderOdpoved($q);                
+	$out .= renderOdpoved($q);
 	$out .= '<input type="hidden" name="hash" value="'.getSetHash().'" /><input type="hidden" name="qnum" value="'.$qn.'" />';
 	return '<div class="priklad">'.$out.'</div>';
-}               
+}
