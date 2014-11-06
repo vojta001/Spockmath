9fbdaae1a1917f1615f94894ec44d8aa6641be34
vojta001 <vojta001@e6a2024a-f257-11e3-b826-311ed6531c3a>
2014-11-06 17:01:44 +0000

    bugfix

--- page/home/render.php
+++ page/home/render.php
@@ -8,14 +8,14 @@
 <form method="post">
 	<?php echo renderSpockQuestion(); ?>
 <?php echo renderQ(); ?>
-<?php if (getSetState() == READ_ONLY) echo ('<input type="text" name="name" placeholder="Zadej svoje jméno:">'); ?>
+<?php if (getSetState() == SADA_READ_ONLY) echo ('<input type="text" name="name" placeholder="Zadej svoje jméno:">'); ?>
 <?php //echo '<pre>'.print_r(getCurrentQ(), 1).getSetHash().</pre>; ?>
 	</pre>
-	<input type="submit" name="submit-prev<?php if (getSetState() == READ_ONLY) echo ('-ro') ?>" value="Předchozí" <?php if (getPosition() == 0) echo 'disabled '; ?>/>
+	<input type="submit" name="submit-prev<?php if (getSetState() == SADA_READ_ONLY) echo ('-ro') ?>" value="Předchozí" <?php if (getPosition() == 0) echo 'disabled '; ?>/>
 <?php if (getPosition() < getQCount()-1): ?>
-	<input type="submit" name="submit-next<?php if (getSetState() == READ_ONLY) echo ('-ro') ?>" value="Další" />
+	<input type="submit" name="submit-next<?php if (getSetState() == SADA_READ_ONLY) echo ('-ro') ?>" value="Další" />
 <?php else: ?>
-	<input type="submit" name="submit-save<?php if (getSetState() == READ_ONLY) echo ('-ro') ?>" value="<?php if (getSetState() == READ_ONLY) echo ('Konec'); else echo ('Uložit'); ?>" />
+	<input type="submit" name="submit-save<?php if (getSetState() == SADA_READ_ONLY) echo ('-ro') ?>" value="<?php if (getSetState() == SADA_READ_ONLY) echo ('Konec'); else echo ('Uložit'); ?>" />
 <?php endif; ?>
 	<input type="submit" name="submit-clear" value="Smazat sadu" onclick="return confirm('Opravdu smazat?')"/>
 </form>
