88be4b44b30f8f46b1d28c44103fafafbf1ce76b
bayermeister <bayermeister@e6a2024a-f257-11e3-b826-311ed6531c3a>
2014-12-05 12:59:11 +0000

    Ještě doplněno zřetězení v SQL. Téma mělo být témata.

--- page/edit/prepare.php
+++ page/edit/prepare.php
@@ -10,16 +10,16 @@ function renderQtab() {
 	$row = 0;
 //	$eo = array('odd', 'even');
 	
-	$qRows = $mysqli->query('SELECT q.id, typ, data2, data, comment, multi, t.jmeno AS tema FROM (`otazka` AS q INNER JOIN `otazka_tema` AS qt ON q.id = qt.otazka_id) INNER JOIN tema AS t ON t.id = qt.tema_id GROUP BY q.id ORDER BY q.id');
+	$qRows = $mysqli->query('SELECT q.id, typ, data2, data, comment, multi, GROUP_CONCAT(t.jmeno SEPARATOR \', \') AS temata FROM (`otazka` AS q INNER JOIN `otazka_tema` AS qt ON q.id = qt.otazka_id) INNER JOIN tema AS t ON t.id = qt.tema_id GROUP BY q.id ORDER BY q.id');
 	if (!$qRows) {
 		flm("Dotaz na vše z `otazka` selhal.", '', MSG_ERROR);
 		return array();
 	}
-	$out = '<table><tr><th>Číslo</th><th>Typ</th><th>Otázka</th><th>Komentář</th><th>Téma</th><th>Multi</th></tr>';
+	$out = '<table><tr><th>Číslo</th><th>Typ</th><th>Otázka</th><th>Komentář</th><th>Témata</th><th>Multi</th></tr>';
 	
 	while($q = $qRows->fetch_object()) {
 		$otazka = ($q->typ == 1) ? $q->data : $q->data2;
-		$out .= '<tr class="'.$eo[($row++)%2].'"><td>'.$q->id.'</td><td>'.$QT_STR[$q->typ].'</td><td>'.$otazka.'</td><td>'.$q->comment.'</td><td>'.$q->tema.'</td><td>'.($q->multi?'Ano':'Ne').'</td></tr>';
+		$out .= '<tr class="'.$eo[($row++)%2].'"><td>'.$q->id.'</td><td>'.$QT_STR[$q->typ].'</td><td>'.$otazka.'</td><td>'.$q->comment.'</td><td>'.$q->temata.'</td><td>'.($q->multi?'Ano':'Ne').'</td></tr>';
 	}
 	
 	$out .= '<tr class="'.$eo[($row++)%2].'"><td colspan="6">Přidat novou</td></tr>';
