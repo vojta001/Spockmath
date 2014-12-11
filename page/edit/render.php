f36f9ff505f8e61e728820f39b82cacea3e467d4
bayermeister <bayermeister@e6a2024a-f257-11e3-b826-311ed6531c3a>
2014-12-11 06:53:34 +0000

    Navenek skoro neviditelné změny. Pokud dorazí neplatný GET editoru, hodí to na 404.
    RgB

--- page/edit/render.php
+++ page/edit/render.php
@@ -1,3 +1,8 @@
 <?php
-echo jarSay('Pojďme vytvářet hodnoty pro naše studenty.<br />Zde je výčet všech otázek v databázi.<br />Můžeš přidat novou, nebo stávající upravit.', JAR_UHURA, true);
-echo renderQtab();
+if (is_int($_id)) {
+	renderQEdit($_id);
+}
+elseif (is_null($_id)) {
+	echo jarSay('Pojďme vytvářet hodnoty pro naše studenty.<br />Zde je výčet všech otázek v databázi.<br />Můžeš přidat novou, nebo stávající upravit.', JAR_UHURA, true);
+	echo renderQtab();
+}
