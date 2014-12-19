dcf8f6b18f524734ddcd39fb42b7869c637c16b0
vojta001 <vojtech.kane@gmail.com>
2014-12-19 14:18:23 +0000

    Postup se stánkou Statistiky. Základní rozvržení, jak to bude fungovat (zobrazí to nějaké obecné (ne)zajímavosti jako třeba kolik testů už bylo vyplněno a zeptá se na jméno, podle něhož vrátí nějaké personalizované statistiky)

---
+++ page/stats/render.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_GET['name']) && $_GET['name'] && is_string($_GET['name'])) {
+	echo(renderPersStats());
+} else {
+	echo(jarSay('Tak ty by jsi chtěl vedět ještě něco víc? Tady to je:', JAR_SPOCK, false));
+	echo(renderStats());
+	echo(jarSay('Jo tobě to ještě nestačio? Zadej mi sem jméno a podíváme se na tebe:', JAR_SPOCK, true));
+	echo(renderNameEdit());
+}
+?>
