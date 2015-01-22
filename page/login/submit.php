a04e135db08afcc90eec34a1f3efb832aecedf3c
vojta001 <vojtech.kane@gmail.com>
2015-01-22 16:37:42 +0000

    Přidal jsem nástroj na tvorbu hashů do login, pokud jsi admin. Zatím neplánuji tvořit nástroj na přidání uživatele. Tohle má jen pomoct, když ho zakládám v admineru. Přesunul jsem pár věcí tak, abychom se, dle mého názoru, více přiblížili původnímu rozdělení. Tzn. udělal jsem funkci isAdmin a přesunul tvorbu tabulky z render do prepare.

--- page/login/submit.php
+++ page/login/submit.php
@@ -5,4 +5,8 @@ require_once 'prepare.php';
 if (isValidLogin($_POST['usrname'], $_POST['passwd'])) {
 	loggIn($_POST['usrname']);
 	flm('Vítejte '.$_POST['usrname']);
+}elseif (isset($_POST['make_hash']) && isset($_POST['tohash']) && $_POST['tohash']) {
+	if (isAdmin(getUser())) {
+		flm (makeHash(), 'Hash vstupu jest:');
+	}
 }
