2d84544fc127b3cb787ce0eaac8fef5b310ac383
vojta001 <vojta001@e6a2024a-f257-11e3-b826-311ed6531c3a>
2015-03-14 18:13:05 +0000

    Dořešeno přijímání jména uživatele z POSTu a id sady s GETu. Tím by měla být stránka výsledky uzavřena až na případné css.

--- page/vysledky/render.php
+++ page/vysledky/render.php
@@ -1,14 +1,17 @@
 <?php
-if (empty($jmeno/*post*/) && empty($q)) {
-	echo (renderSadaTable('Příliš \'); DROP TABLE sada; -- žluťoučký kůň úpěl ďábelské ódy.'));
-    prepareSetById(85);
+$jmeno = isset($_SESSION['vysledky']['user'])?$_SESSION['vysledky']['user']:null;
+if (empty($jmeno) && empty($_GET['q'])) {
 	echo jarSay('Chcete se podívat, jak některý ze studentů dopadl? Jen sem zadejte jeho jméno.', JAR_UHURA, TRUE); ?>
-	<form method="get">
+	<form method="post">
 	<input type="text" name="jmeno" placeholder="Jméno, které student zadal při ukládání">
 	<input type="submit">
 	</form>
-<?php } elseif (!empty($jmeno) && empty($q)) {
-	flm('zadal\'s jméno '.$_GET['jmeno']);
-} elseif (!empty($q)) {
-
+<?php } elseif (!empty($jmeno) && empty($_GET['q'])) {
+	flm('zadal\'s jméno '.$jmeno);
+	echo (renderSadaTable($jmeno));
+} elseif (!empty($_GET['q'])) {
+	unset($_SESSION['vysledky']['user']);
+	prepareSetById($_GET['q']);
+	header('Location: '.WEB_ROOT.'home');
+	exit;
 }
