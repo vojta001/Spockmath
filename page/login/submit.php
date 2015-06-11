0b0ddf38e044e181b9648f55d7ec0e18443f247c
vojta001 <vojtech.kane@gmail.com>
2015-06-11 15:52:59 +0000

    Zpřehlednění submitu stránky login. Identifikace nesprávných přihlašovacích údajů.

--- page/login/submit.php
+++ page/login/submit.php
@@ -2,12 +2,14 @@
 
 require_once 'prepare.php';
 
-if (isValidLogin($_POST['usrname'], $_POST['passwd'])) {
-	loggIn($_POST['usrname']);
-	flm('Vítejte '.$_POST['usrname'], '', MSG_INFO);
-}
-elseif (isset($_POST['make_hash']) && isset($_POST['tohash']) && $_POST['tohash']) {
-	if (isAdmin(getUser())) {
+if (loggedIn()) {
+	if (isset($_POST['make_hash']) && !empty($_POST['tohash']) && isAdmin(getUser()))
 		flm(makeHash($_POST['tohash']), 'Hash vstupu jest:', MSG_INFO);
+} elseif (!empty($_POST['usrname'])) {
+	if (isValidLogin($_POST['usrname'], $_POST['passwd'])) {
+		loggIn($_POST['usrname']);
+		flm('Vítejte '.$_POST['usrname'], '', MSG_INFO);
+	} else{
+		flm('Neplatné přihlašovací údaje', 'Přihlášení', MSG_WARNING);
 	}
 }
